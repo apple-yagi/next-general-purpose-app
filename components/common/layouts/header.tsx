35c20d48388ee8158b3a8f2b7a2bdd8554227f38
apple-yagi <applesryu@gmail.com>
2020-12-18 13:46:42 +0900

    add layouts

--- components/common/layouts/header.tsx
+++ components/common/layouts/header.tsx
@@ -1,15 +1,15 @@
 import Link from 'next/link'
-import '../../../styles/common/layouts/Header.module.css'
+import classes from '../../../styles/common/layouts/Header.module.css'
 
 export default function Header() {
   return (
-    <div className="header">
-      <div className="header-body">
+    <div className={classes.header}>
+      <div className="container">
         <Link href="/">
-          <a>Home</a>
+          <a className={classes.header_link}>Home</a>
         </Link>
         <Link href="/discussion">
-          <a>Discussion</a>
+          <a className={classes.header_link}>Discussion</a>
         </Link>
       </div>
     </div>
